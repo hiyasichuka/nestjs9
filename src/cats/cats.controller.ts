5924e3846b5d5ca051a93a81b1a06d077209249b
Kei Takahashi <takahashi.kgs@om.asahi-kasei.co.jp>
2022-08-12 14:22:41 +0900

    Add docs and query

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -3,7 +3,9 @@ import {
   Get,
   Header,
   HttpCode,
+  Param,
   Post,
+  Query,
   Redirect,
   Req,
 } from '@nestjs/common';
@@ -12,9 +14,23 @@ import { Request } from 'express';
 @Controller('cats')
 export class CatsController {
   @Get()
-  @Redirect('https://nestjs.com', 301)
   findAll(@Req() request: Request): string {
     return 'This action returns all cats';
+  }
+
+  @Get('docs')
+  @Redirect('https://docs.nestjs.com', 302)
+  getDocs(@Query('version') version) {
+    console.log('version');
+    console.log(version);
+    if (version && version === '5') {
+      return { url: 'https://docs.nestjs.com/v5/' };
+    }
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string): string {
+    return `This action returns a #${id} cat`;
   }
 
   @Post('code')
