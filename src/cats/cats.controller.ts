dadadfd02de66df1ccd1817857f8450d0a028c82
Kei Takahashi <takahashi.kgs@om.asahi-kasei.co.jp>
2022-08-12 14:16:04 +0900

    Add post and redirect

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,10 +1,31 @@
-import { Controller, Get, Req } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Header,
+  HttpCode,
+  Post,
+  Redirect,
+  Req,
+} from '@nestjs/common';
 import { Request } from 'express';
 
 @Controller('cats')
 export class CatsController {
   @Get()
+  @Redirect('https://nestjs.com', 301)
   findAll(@Req() request: Request): string {
     return 'This action returns all cats';
+  }
+
+  @Post('code')
+  @HttpCode(204)
+  createPost() {
+    return 'This action adds a new cat';
+  }
+
+  @Post()
+  @Header('Cache-Control', 'none')
+  create() {
+    return 'This action adds a new cat';
   }
 }
